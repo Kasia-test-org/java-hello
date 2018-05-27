84a62f20cc7d7f0b7ffa7dbb4a2be894dd11672e
KasiaWa <38590221+KasiaWa@users.noreply.github.com>
2018-05-27 12:35:49 +0100

    upload file
    added hello file to repository

---
+++ Hello.java
@@ -0,0 +1,23 @@
+//Programmer:Katarzyna Wickowska
+//Student nr:B00100585
+//Date written:22/09/2017
+//Function: Exercise
+
+//program display "hello java", first program
+
+//open class
+class Hello
+{
+	//open main method which runs a program
+	public static void main(String[]args)
+	{
+		//Display "Hello World"
+		System.out.print("Hello World");
+        System.out.print("This is my first Java Program");
+        hello();
+    }//end of main
+    public static void hello()
+    {
+		System.out.print("Hello from method");
+	}
+}//end of class
